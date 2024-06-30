4edd6c6fe1eb7e1d633b621caba124f35c5ad7fe
SUMITpatil2021 <sumit.patil2021@gmail.com>
2024-07-01 01:00:05 +0530

    changes in app.js and index.js file

--- client_side/myreactapp/src/App.js
+++ client_side/myreactapp/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginForm from './components/LoginForm';
+import StudentTable from './components/Student';
+import MyRegisterForm from './components/RegMongo';
+import { Route, Router, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+          <Route path="/" element={<LoginForm />}/>
+          <Route path="/dashboard" element={<StudentTable />} />
+          <Route path="/signup" element={<MyRegisterForm/>}/>
+          </Routes> 
     </div>
   );
 }
